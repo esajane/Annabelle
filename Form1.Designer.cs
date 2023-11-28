ea921eb39d89068212ccbbc9e6f82a1cfd70ec55
aexizn <janessa.pandiyan@cit.edu>
2023-11-28 20:26:59 +0800

    Commiting murder

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace DIP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxWebcam = new System.Windows.Forms.PictureBox();
+            this.pictureBoxBackground = new System.Windows.Forms.PictureBox();
+            this.pictureBoxForeground = new System.Windows.Forms.PictureBox();
+            this.pictureBoxResult = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.captureImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageSubtractionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWebcam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBackground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxForeground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResult)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBoxWebcam
+            // 
+            this.pictureBoxWebcam.Location = new System.Drawing.Point(25, 77);
+            this.pictureBoxWebcam.Name = "pictureBoxWebcam";
+            this.pictureBoxWebcam.Size = new System.Drawing.Size(636, 327);
+            this.pictureBoxWebcam.TabIndex = 7;
+            this.pictureBoxWebcam.TabStop = false;
+            // 
+            // pictureBoxBackground
+            // 
+            this.pictureBoxBackground.Location = new System.Drawing.Point(690, 77);
+            this.pictureBoxBackground.Name = "pictureBoxBackground";
+            this.pictureBoxBackground.Size = new System.Drawing.Size(636, 327);
+            this.pictureBoxBackground.TabIndex = 8;
+            this.pictureBoxBackground.TabStop = false;
+            // 
+            // pictureBoxForeground
+            // 
+            this.pictureBoxForeground.Location = new System.Drawing.Point(25, 410);
+            this.pictureBoxForeground.Name = "pictureBoxForeground";
+            this.pictureBoxForeground.Size = new System.Drawing.Size(636, 327);
+            this.pictureBoxForeground.TabIndex = 9;
+            this.pictureBoxForeground.TabStop = false;
+            // 
+            // pictureBoxResult
+            // 
+            this.pictureBoxResult.Location = new System.Drawing.Point(690, 410);
+            this.pictureBoxResult.Name = "pictureBoxResult";
+            this.pictureBoxResult.Size = new System.Drawing.Size(636, 327);
+            this.pictureBoxResult.TabIndex = 10;
+            this.pictureBoxResult.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.captureImageToolStripMenuItem,
+            this.loadImageToolStripMenuItem,
+            this.imageSubtractionToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1370, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // captureImageToolStripMenuItem
+            // 
+            this.captureImageToolStripMenuItem.Name = "captureImageToolStripMenuItem";
+            this.captureImageToolStripMenuItem.Size = new System.Drawing.Size(97, 20);
+            this.captureImageToolStripMenuItem.Text = "Capture Image";
+            this.captureImageToolStripMenuItem.Click += new System.EventHandler(this.captureImageToolStripMenuItem_Click);
+            // 
+            // loadImageToolStripMenuItem
+            // 
+            this.loadImageToolStripMenuItem.Name = "loadImageToolStripMenuItem";
+            this.loadImageToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.loadImageToolStripMenuItem.Text = "Load Image";
+            this.loadImageToolStripMenuItem.Click += new System.EventHandler(this.loadImageToolStripMenuItem_Click);
+            // 
+            // imageSubtractionToolStripMenuItem
+            // 
+            this.imageSubtractionToolStripMenuItem.Name = "imageSubtractionToolStripMenuItem";
+            this.imageSubtractionToolStripMenuItem.Size = new System.Drawing.Size(116, 20);
+            this.imageSubtractionToolStripMenuItem.Text = "Image Subtraction";
+            this.imageSubtractionToolStripMenuItem.Click += new System.EventHandler(this.imageSubtractionToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Controls.Add(this.pictureBoxResult);
+            this.Controls.Add(this.pictureBoxForeground);
+            this.Controls.Add(this.pictureBoxBackground);
+            this.Controls.Add(this.pictureBoxWebcam);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWebcam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBackground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxForeground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResult)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBoxWebcam;
+        private System.Windows.Forms.PictureBox pictureBoxBackground;
+        private System.Windows.Forms.PictureBox pictureBoxForeground;
+        private System.Windows.Forms.PictureBox pictureBoxResult;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem captureImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem imageSubtractionToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace DIP
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        private Bitmap backgroundImage;
+        private Bitmap foregroundImage;
+        private Bitmap resultImage;
+        private Color mygreen = Color.FromArgb(0, 255, 0);
+        private int threshold = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeWebcam();
+        }
+
+        private void InitializeWebcam()
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (videoDevices.Count > 0)
+            {
+                videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+                videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+                videoSource.Start();
+            }
+        }
+
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            
+            pictureBoxWebcam.Image = (Bitmap)eventArgs.Frame.Clone();
+            pictureBoxWebcam.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void CaptureImage()
+        {
+            
+            if (pictureBoxWebcam.Image != null)
+            {
+               
+                if (backgroundImage != null)
+                {
+                    backgroundImage.Dispose();
+                }
+
+               
+                backgroundImage = new Bitmap(pictureBoxWebcam.Image);
+
+                
+                if (pictureBoxBackground.Image != null)
+                {
+                    pictureBoxBackground.Image.Dispose();
+                }
+
+               
+                pictureBoxBackground.Image = backgroundImage;
+                pictureBoxBackground.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                MessageBox.Show("Failed to capture image from webcam.");
+            }
+        }
+
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+            }
+        }
+
+
+
+        private void captureImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CaptureImage();
+        }
+
+        private void loadImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Open Image";
+                dlg.Filter = "Image files (*.bmp, *.jpg, *.jpeg, *.png)|*.bmp;*.jpg;*.jpeg;*.png";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    foregroundImage = new Bitmap(dlg.FileName);
+                    pictureBoxForeground.Image = foregroundImage;
+                    pictureBoxForeground.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+            }
+        }
+
+        private void imageSubtractionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            pictureBoxBackground = new PictureBox();
+            pictureBoxBackground.Image = backgroundImage;
+            pictureBoxForeground = new PictureBox();
+            pictureBoxForeground.Image = foregroundImage;
+
+            if (backgroundImage != null && foregroundImage != null)
+            {
+
+                if (backgroundImage.Size != foregroundImage.Size)
+                {
+                    foregroundImage = new Bitmap(foregroundImage, backgroundImage.Size);
+                }
+                Color chromaKeyColor = Color.FromArgb(0, 255, 0);
+                int tolerance = 150; 
+                resultImage = new Bitmap(foregroundImage.Width, foregroundImage.Height);
+
+                for (int x = 0; x < foregroundImage.Width; x++)
+                {
+                    for (int y = 0; y < foregroundImage.Height; y++)
+                    {
+                        Color fgColor = foregroundImage.GetPixel(x, y);
+                        if (IsChroma(fgColor, chromaKeyColor, tolerance))
+                        {
+                            
+                            Color bgColor = backgroundImage.GetPixel(x, y);
+                            resultImage.SetPixel(x, y, bgColor);
+                        }
+                        else
+                        {
+                            
+                            resultImage.SetPixel(x, y, fgColor);
+                        }
+                    }
+                }
+
+                pictureBoxResult.Image = resultImage;
+                pictureBoxResult.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                MessageBox.Show("Please capture the background and load the foreground image first.");
+            }
+        }
+
+        private bool IsChroma(Color color, Color chromaKeyColor, int tolerance)
+        {
+            return Math.Abs(color.R - chromaKeyColor.R) < tolerance &&
+                   Math.Abs(color.G - chromaKeyColor.G) < tolerance &&
+                   Math.Abs(color.B - chromaKeyColor.B) < tolerance;
+        }
+    }
+
+}
+
